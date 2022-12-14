c07412431fcbac69792117b55e64028c5e7cc4bb
Quan Phung <quan.phung@eastgata-software.com>
2022-12-14 21:37:13 +0700

    update sprint 3

--- src/main/java/quan/hust/itjapanese/services/BookServiceImpl.java
+++ src/main/java/quan/hust/itjapanese/services/BookServiceImpl.java
@@ -1,8 +1,7 @@
 package quan.hust.itjapanese.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,15 +13,13 @@ import com.querydsl.core.types.OrderSpecifier;
 import quan.hust.itjapanese.converter.BookConverter;
 import quan.hust.itjapanese.converter.CommentConverter;
 import quan.hust.itjapanese.domain.Book;
-import quan.hust.itjapanese.domain.Comment;
 import quan.hust.itjapanese.dto.BookDto;
-import quan.hust.itjapanese.dto.CommentDto;
-import quan.hust.itjapanese.dto.response.GetCommentResponse;
 import quan.hust.itjapanese.dto.filter.BookFilter;
 import quan.hust.itjapanese.dto.orderutil.BookOrderUtil;
 import quan.hust.itjapanese.dto.request.AddBookRequest;
 import quan.hust.itjapanese.dto.request.GetBookRequest;
 import quan.hust.itjapanese.dto.response.AddBookResponse;
+import quan.hust.itjapanese.dto.response.CompareBookResponse;
 import quan.hust.itjapanese.dto.response.GetBookResponse;
 import quan.hust.itjapanese.exception.InvalidSortColumnException;
 import quan.hust.itjapanese.exception.InvalidSortOrderException;
@@ -72,6 +69,9 @@ public class BookServiceImpl implements BookService
     GetBookResponse response;
     try
     {
+      Integer size = request.getSize();
+      Integer page = request.getPage();
+
       BookFilter filter = BookFilter.builder()
         .id(request.getId())
         .minPrice(request.getMinPrice())
@@ -83,7 +83,7 @@ public class BookServiceImpl implements BookService
         .build();
 
       OrderSpecifier<String>[] orders = BookOrderUtil.createOrderBy(request.getOrderBy());
-      List<Book> books = bookRepository.getBooksByFilter(filter,orders);
+      List<Book> books = bookRepository.getBooksByFilter(filter,orders, size, page );
       List<BookDto> bookDtos = converter.convertToDtoList(books);
 
       long count = bookRepository.count(filter);
@@ -108,6 +108,28 @@ public class BookServiceImpl implements BookService
     List<Book> books = csvHelper.csvToBooks(file);
     bookRepository.saveAll(books);
     return true;
+  }
+
+  @Override
+  public CompareBookResponse compare(Integer id1, Integer id2)
+  {
+    CompareBookResponse response = null;
+    Optional<Book> book1Opt = bookRepository.findById(id1);
+    Optional<Book> book2Opt = bookRepository.findById(id2);
+
+    if(book1Opt.isPresent() && book2Opt.isPresent())
+    {
+       Book book1 = book1Opt.get();
+       Book book2 = book2Opt.get();
+
+       BookDto book1Dto = converter.convertToDto(book1);
+       BookDto book2Dto = converter.convertToDto(book2);
+       response = CompareBookResponse.builder()
+         .firstBook(book1Dto)
+         .secondBook(book2Dto)
+         .build();
+    }
+    return response;
   }
 
 }
